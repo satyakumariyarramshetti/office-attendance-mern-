6a77135a621343f28e48973d1244c24ab16ed837
GowthamiRayudu07 <gowthamirayudu2624@gmail.com>
2026-05-04 10:12:06 +0530

    password

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -15,7 +15,7 @@ const AdminLogin = ({ setIsAdmin }) => {
 
 
     // Hardcoded credentials for now (can replace with backend validation later)
-    if (trimmedUsername === 'W0rk10g@dm!n' && trimmedPassword === 'Ps@dm!n#241219') {
+    if (trimmedUsername === 'W0rkl0g@dm!n' && trimmedPassword === 'Ps@dm!n#241219') {
       localStorage.setItem('adminToken', 'true'); // simulate login
       setIsAdmin(true);
       navigate('/admin');
